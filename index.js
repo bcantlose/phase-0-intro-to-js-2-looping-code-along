d0d4a12b08166bc487a5f756da1f90d709fc6a03
Brian <bcantlose@gmail.com>
2023-03-13 16:16:45 -0500

    Javascript is the Bane of my existence

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(name, value){
+let myArray = []
+for(let i=0; i < name.length; i++) {
+    myArray.push(`Thank you, ${name[i]}, for the wonderful ${value} gift!`)
+}
+return myArray}
+
+function countDown(i){
+    while(i > -1)
+    {
+        console.log(i);
+        i--;
+    
+    }
+}
+
+countDown();
+console.log(countDown);
